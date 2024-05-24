88f526dbdc7259ea691398bab5da68ec1845e1e4
macs1756 <kindrativmaksum312@gmail.com>
2024-05-24 12:17:09 +0300

    v2

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,87 +2,11 @@ import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 
 const HomeData = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      strapiBestOfCategory(slug: {eq: "best-vape-accessories"}) {
-        name
-        slug
-        subTitle
-        introTitle
-        id
-        editorProduct {
-          couponCode
-          couponText
-          couponRedirect
-          name
-          rating
-          id
-          affiliateLevel2Editor
-          affiliateLink
-          link
-          image {
-            alternativeText
-            height
-            width
-            url
-          }
-        }
-        introText {
-          data {
-            introText
-          }
-        }
-        best_ofs {
-          name
-          id
-          slug
-          shortDescription
-          product1 {
-            ...product
-          }
-          product2 {
-            ...product
-          }
-          product3 {
-            ...product
-          }
-        }
-        introImage {
-          alternativeText
-          height
-          width
-          url
-        }
-        updatedAt
-        seo {
-          metaTitle
-          metaDescription
-          metaRobots
-          canonicalURL
-          keywords
-          metaImage {
-            url
-            mime
-          }
-          metaSocial {
-            title
-            socialNetwork
-            description
-            image {
-              url
-              mime
-            }
-          }
-        }
-      }
-    }
-  `);
-
-  console.log(data);
+  
  
   return (
     <div>
-      {/* Render your component */}
+      Working
     </div>
   );
 };
