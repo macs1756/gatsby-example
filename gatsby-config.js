45b43725abed4cedc854d249085b7172afa7986a
macs1756 <kindrativmaksum312@gmail.com>
2024-05-28 08:41:22 +0300

    add env files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,8 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+
 module.exports = {
   plugins: [
     {
@@ -16,7 +21,7 @@ module.exports = {
       resolve: 'gatsby-source-strapi',
       options: {
         apiURL: 'http://127.0.0.1:1337',
-        accessToken: '1247cce3db238d58a1ff7ef66a32ca4dd85fc4334cfb0bdea27b70fcff247f0fb4eed518a8e979669c5272a769a610937a35dd173309ab1a490baf136257ac914b34e921ae5391fac4b79af4dc7e97117b1a16d1c16dd37f8a0278039eb14bc40ef132ed613c6cbad111d31fee255df4b38fa3971116a478dc7bbe3b798bf86e',
+        accessToken: process.env.GRAPHQL_TOKEN,
       }
     }
   ],
--- src/themes/home/index.jsx
+++ src/themes/home/index.jsx
@@ -1,9 +1,11 @@
-import '../../styles/style'
+import '../../styles/style.css'
 import React from 'react';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div
+      className='flex w-full h-[100vh] items-center justify-center text-[41px]'
+    >Home</div>
   );
 }
 
